4c7e3bb2ecb95d654929c1c9665e7a182f46d35f
Mit Mathukiya <54717452+Mitpatel13@users.noreply.github.com>
2024-02-06 23:26:36 +0530

    update user

--- controller/userContoller.js
+++ controller/userContoller.js
@@ -18,12 +18,54 @@ module.exports = {
     getAllUser:async(req,res)=>{
         try{
             var users = await User.find();
-            res.status(201).json({ message: 'All user get successfully!' ,data:users});
+            res.status(200).json({ message: 'All user get successfully!' ,data:users});
 
 
         }
         catch (err) {
             res.status(500).json({ error: err.message });
+        }
+    },
+
+    updateUser: async (req, res) => {
+        try {
+            
+            const adminUser = await User.findById(req.params.id);
+            if (!adminUser.isAdmin) {
+                return res.status(403).json({ error: 'Unauthorized access' });
+            }
+            const { id, mobileNo } = req.body;
+            const user = await User.findById(id);
+
+            if (!user) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+            user.mobileNo = mobileNo;
+            await user.save();
+
+            res.status(200).json({ message: 'User mobile number updated successfully' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    },
+    deleteUser: async (req, res) => {
+        try {
+            const adminUser = await User.findById(req.params.id);
+            if (!adminUser.isAdmin) {
+                return res.status(403).json({ error: 'Unauthorized access' });
+            }
+            const { id,} = req.body;
+            const user = await User.findByIdAndDelete(id);
+
+            if (!user) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+
+            res.status(200).json({ message: 'User deleted successfully' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal server error' });
         }
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controller/userContoller');
 
 router.post('/register', userController.registerUser);
-router.get('/register', userController.getAllUser);
-
+router.get('/allUser', userController.getAllUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 module.exports = router;
