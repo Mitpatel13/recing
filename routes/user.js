ac91742d11e289fad1f4196cc325f29db2b153d4
Mit Mathukiya <54717452+Mitpatel13@users.noreply.github.com>
2024-02-07 11:14:35 +0530

    add router

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,5 @@ router.post('/register', userController.registerUser);
 router.get('/allUser', userController.getAllUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+
 module.exports = router;
